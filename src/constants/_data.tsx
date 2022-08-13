938c998a1f340990c16a9d65d7cc2116fa702dbd
vova <lismgmk2@gmail.com>
2022-08-13 22:32:23 +0300

    fix deploy img

--- src/constants/_data.tsx
+++ src/constants/_data.tsx
@@ -19,19 +19,19 @@ export const headerTitles = {
   header: {
     title: ' Юрий Поклад: «Динамо» как бы начинает работать заново',
     tag: '#болельщикам',
-    pathImg: '/header_bg.png',
+    pathImg: './header_bg.png',
     article: '',
   },
   middle: {
     title: ' Юрий Поклад: «Динамо» как бы начинает работать заново',
     tag: '#Экстрим',
-    pathImg: '/middle_bg.png',
+    pathImg: './middle_bg.png',
     article: '',
   },
   footer: {
     title: 'Экстрим',
     tag: '',
-    pathImg: '/footer_bg.png',
+    pathImg: './footer_bg.png',
     article: 'Интервью с Юрием Покладом.Полный список всех медалей Беларуси на Европейских играх.',
   },
 };
@@ -61,7 +61,7 @@ export const lowPages = [
 
 export const cards: cardsType[] = [
   {
-    pathImg: '/post_1.png',
+    pathImg: './post_1.png',
     fullImg: false,
     data: 'Сегодня',
     tag: '#болельщикам',
@@ -70,7 +70,7 @@ export const cards: cardsType[] = [
       'И если для «горняков», которые неделей ранее разгромно уступили в Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск — формальность, то от «жёлто-синих»...',
   },
   {
-    pathImg: '/post_2.png',
+    pathImg: './post_2.png',
     fullImg: false,
     data: '09.07.19',
     tag: '#любительский_спорт',
@@ -79,7 +79,7 @@ export const cards: cardsType[] = [
       'И если для «горняков», которые неделей ранее разгромно уступили в Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск — формальность, то от «жёлто-синих»...',
   },
   {
-    pathImg: '/post_1.png',
+    pathImg: './post_1.png',
     fullImg: true,
     data: '09.07.19',
     tag: '#Профессиональный_спорт',
@@ -88,7 +88,7 @@ export const cards: cardsType[] = [
       'И если для «горняков», которые неделей ранее разгромно уступили в Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск — формальность, то от «жёлто-синих»...',
   },
   {
-    pathImg: '/post_2.png',
+    pathImg: './post_2.png',
     fullImg: false,
     data: 'Сегодня',
     tag: '#болельщикам',
@@ -99,7 +99,7 @@ export const cards: cardsType[] = [
 ];
 export const cardsSlice2 = [
   {
-    pathImg: '/post_1.png',
+    pathImg: './post_1.png',
     fullImg: true,
     data: '09.07.19',
     tag: '#Профессиональный_спорт',
@@ -108,7 +108,7 @@ export const cardsSlice2 = [
       'И если для «горняков», которые неделей ранее разгромно уступили в Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск — формальность, то от «жёлто-синих»...',
   },
   {
-    pathImg: '/post_2.png',
+    pathImg: './post_2.png',
     fullImg: false,
     data: 'Сегодня',
     tag: '#болельщикам',
@@ -125,19 +125,19 @@ export const avatars = [
     conversation: 'Сергей Корниленко станет играющим тренером «Крыльев советов»',
   },
   {
-    pathImg: '/avatar_2.png',
+    pathImg: './avatar_2.png',
     name: 'Алексей Михайличенко',
     occupation: 'Главный тренер ф.к. Динамо',
     conversation: '«Динамо» как бы начинает работать заново',
   },
   {
-    pathImg: '/avatar_3.png',
+    pathImg: './avatar_3.png',
     name: 'Алексей Михайличенко',
     occupation: 'Главный тренер ф.к. Динамо',
     conversation: 'Сергей Корниленко станет играющим тренером «Крыльев советов»',
   },
   {
-    pathImg: '/avatar_1.png',
+    pathImg: './avatar_1.png',
     name: 'Алексей Михайличенко',
     occupation: 'Главный тренер ф.к. Динамо',
     conversation: '«Динамо» как бы начинает работать заново',
